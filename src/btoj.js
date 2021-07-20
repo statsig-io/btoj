2c1dc71b01494c9ecb63472bcea3a265874377a2
roim <roim.rodrigo@gmail.com>
2021-07-19 19:49:23 -0700

    Add compression

--- src/btoj.js
+++ src/btoj.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
-const encodings = ['latin1', 'utf16le', 'ucs2', 'utf8', 'ascii', 'base64', 'base64url', 'hex'];
+const encodings = [
+  "latin1",
+  "utf16le",
+  "ucs2",
+  "utf8",
+  "ascii",
+  "base64",
+  "base64url",
+  "hex",
+];
+
 const yargs = require("yargs");
 const path = require("path");
 const fs = require("fs");
+const z = require("zlib");
 const { exit } = require("process");
 
 function getDelimiter(s) {
-  return (s.match(/\'/g)||[]).length >= (s.match(/\"/g)||[]).length ? '"' : "'";
+  return (s.match(/\'/g) || []).length >= (s.match(/\"/g) || []).length
+    ? '"'
+    : "'";
 }
 
 const argv = yargs
@@ -21,9 +34,17 @@ const argv = yargs
     required: true,
     type: "string",
   })
+  .option("compress", {
+    alias: "z",
+    description:
+      "Use NodeJS compression libraries.  The generated module will automatically decompress when exporting.`",
+    required: false,
+    type: "boolean",
+  })
   .option("staging", {
     alias: "s",
-    description: "Path to the staging directory, used to measure size of different encodings.  Defaults to `.btoj/`",
+    description:
+      "Path to the staging directory, used to measure size of different encodings.  Defaults to `.btoj/`",
     required: false,
     type: "string",
   })
@@ -36,23 +57,30 @@ if (!inputPath) {
   exit(1);
 }
 
-const stagingDir = path.resolve(argv.staging ?? '.btoj/');
-if (fs.existsSync(stagingDir)){
+const stagingDir = path.resolve(argv.staging ?? ".btoj/");
+if (fs.existsSync(stagingDir)) {
   console.error(`Staging directory '${stagingDir}' already exists.  Aborting.`);
   exit(1);
 }
 
 fs.mkdirSync(stagingDir);
 
-const data = fs.readFileSync(inputPath);
+const input = fs.readFileSync(inputPath);
 const possibleEncodings = {};
 for (const e of encodings) {
-  const encodedFile = path.resolve(stagingDir, `${e}.js`);
-  const d = fs.readFileSync(inputPath, e);
-  const delimiter = getDelimiter(d);
-  const encoded = d
+  let encodedInput;
+  if (argv.compress) {
+    encodedInput = z
+      .brotliCompressSync(fs.readFileSync(inputPath, null))
+      .toString(e);
+  } else {
+    encodedInput = fs.readFileSync(inputPath, e);
+  }
+
+  const d = getDelimiter(encodedInput);
+  const encoded = encodedInput
     .replaceAll("\\", "\\\\")
-    .replaceAll(delimiter, `\\${delimiter}`)
+    .replaceAll(d, `\\${d}`)
     .replaceAll("\r", "\\r")
     .replaceAll("\b", "\\b")
     .replaceAll("\f", "\\f")
@@ -60,30 +88,54 @@ for (const e of encodings) {
     .replaceAll("\t", "\\t")
     .replaceAll("\n", "\\n");
 
-  fs.writeFileSync(
-    encodedFile,
-    `module.exports = Buffer.from(${delimiter}${encoded}${delimiter}, ${delimiter}${e}${delimiter});\n`
-  );
+  let out = "";
+  if (argv.compress) {
+    out += `let z=require(${d}zlib${d});`;
+  }
 
+  let hardcodedData = `Buffer.from(${d}${encoded}${d}, ${d}${e}${d})`;
+  if (argv.compress) {
+    hardcodedData = `z.brotliDecompressSync(${hardcodedData})`;
+  }
+
+  out += `module.exports=${hardcodedData};\n`;
+
+  const encodedOutFile = path.resolve(stagingDir, `${e}.js`);
+  fs.writeFileSync(encodedOutFile, out);
   try {
-    if (Buffer.compare(require(encodedFile), data) === 0) {
-      possibleEncodings[e] = fs.statSync(encodedFile).size;
+    if (Buffer.compare(require(encodedOutFile), input) === 0) {
+      const size = fs.statSync(encodedOutFile).size;
+      console.debug(`  Encoding '${e}' requires ${size} bytes.`);
+      possibleEncodings[e] = size;
+    } else {
+      console.debug(`  Encoding '${e}' would cause corruption.`);
+      possibleEncodings[e] = Number.MAX_VALUE;
     }
-  } catch (e) {}
-}
-
-let bestEncoding;
-let bestSize = Number.MAX_VALUE;
-for (let e of encodings) {
-  if (possibleEncodings[e] < bestSize) {
-    bestEncoding = e;
-    bestSize = possibleEncodings[e];
+  } catch (ex) {
+    console.debug(`  Encoding '${e}' fails due to ${ex.code}`);
   }
 }
 
-fs.copyFileSync(path.resolve(stagingDir, `${bestEncoding}.js`), path.resolve(argv.output));
-fs.rmSync(stagingDir, { recursive: true, force: true });
+try {
+  let bestEncoding = undefined;
+  let bestSize = Number.MAX_VALUE;
+  for (let e of encodings) {
+    if (possibleEncodings[e] < bestSize) {
+      bestEncoding = e;
+      bestSize = possibleEncodings[e];
+    }
+  }
 
-console.info(
-  `Generated '${argv.output}'.  You can remove '${inputPath}' from your project, the .js file is all you need.`
-);
+  fs.copyFileSync(
+    path.resolve(stagingDir, `${bestEncoding}.js`),
+    path.resolve(argv.output)
+  );
+
+  console.info(
+    `Done!  You can remove '${inputPath}' from your project, '${argv.output}' is all you need.`
+  );
+  fs.rmSync(stagingDir, { recursive: true, force: true });
+} catch (ex) {
+  console.error(ex);
+  fs.rmSync(stagingDir, { recursive: true, force: true });
+}
